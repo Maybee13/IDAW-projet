6fb50a79377df36dbd7d3ea8b15aba668fae7f13
DIDON Maya <maya.didon@etu.imt-lille-douai.fr>
2021-03-29 19:33:28 +0200

    ajout nutrients

--- frontend/aliments.php
+++ frontend/aliments.php
@@ -67,11 +67,119 @@
                         <form id="formutil" onsubmit="event.preventDefault();utilBuildTableRow();" autocomplete="off">
                             <div class="form-group">
                                 <label for="label"> Aliment </label>
-                                <input type="text" class="form-control" value="Skywalker" id="label" />
+                                <input type="text" class="form-control" value="Banane" id="label" />
                             </div>
                             <div class="form-group">
                                 <label for="type"> Type </label>
-                                <input type="text" class="form-control" value="Luc" id="type" />
+                                <input type="text" class="form-control" value="Fruit" id="type" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Energie (kcal/100g) </label>
+                                <input type="text" class="form-control" id="energie" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Eau (g/100g) </label>
+                                <input type="text" class="form-control" id="eau" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Protéines (g/100g) </label>
+                                <input type="text" class="form-control" id="proteines" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Glucides (g/100g) </label>
+                                <input type="text" class="form-control" id="glucides" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Lipides (g/100g) </label>
+                                <input type="text" class="form-control" id="lipides" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Sucres (g/100g) </label>
+                                <input type="text" class="form-control" id="sucres" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Glucose (g/100g) </label>
+                                <input type="text" class="form-control" id="glucose" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Fibres Alimentaires (g/100g) </label>
+                                <input type="text" class="form-control" id="fibres_alimentaires" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Cholestérol (mg/100g) </label>
+                                <input type="text" class="form-control" id="cholesterol" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Calcium (mg/100g) </label>
+                                <input type="text" class="form-control" id="calcium" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Fer (mg/100g) </label>
+                                <input type="text" class="form-control" id="fer" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Iode (µg/100g) </label>
+                                <input type="text" class="form-control" id="iode" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Magnésium (mg/100g) </label>
+                                <input type="text" class="form-control" id="magnesium" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Phosphore (mg/100g) </label>
+                                <input type="text" class="form-control" id="phosphore" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Potassium (mg/100g) </label>
+                                <input type="text" class="form-control" id="potassium" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Sodium (mg/100g) </label>
+                                <input type="text" class="form-control" id="sodium" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines D (µg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_d" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines E (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_e" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines K1 (µg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_k1" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines C (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_c" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B1 (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b1" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B2 (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b2" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B3 (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b3" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B5 (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b5" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B6 (mg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b6" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B9 (µg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b9" />
+                            </div>
+                            <div class="form-group">
+                                <label for="type"> Vitamines B12 (µg/100g) </label>
+                                <input type="text" class="form-control" id="vitamines_b12" />
                             </div>
                             <div class="col-xs-12">
                                 <input type="submit" id="updateButton" class="btn btn-primary" onclick="update();" value="Ajouter">
